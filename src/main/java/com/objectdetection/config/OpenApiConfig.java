dc42226055ba957e230f2288959b0e732caa82ed
EliorMauda <155019365+EliorMauda@users.noreply.github.com>
2025-06-11 16:15:39 +0300

    Update OpenApiConfig.java

--- src/main/java/com/objectdetection/config/OpenApiConfig.java
+++ src/main/java/com/objectdetection/config/OpenApiConfig.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class OpenApiConfig {
@@ -22,7 +25,7 @@ public class OpenApiConfig {
                                 .url("https://github.com/yourusername/ImageObjectDetectionProject"))
                         .license(new License()
                                 .name("MIT License")
-                                .url("https://opensource.org/licenses/MIT")));
+                                .url("https://opensource.org/licenses/MIT")))
                 .servers(Arrays.asList(
                         new Server()
                                 .url("https://object-detection-api-production.up.railway.app")
